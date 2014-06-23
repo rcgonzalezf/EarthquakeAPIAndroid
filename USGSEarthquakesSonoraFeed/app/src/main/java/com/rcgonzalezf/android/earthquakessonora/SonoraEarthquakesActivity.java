e69b39e331105596ece420cf7010dc072af0eeda
rgonzalez <rgonzalez@rgonzalezs-MacBook-Pro.local>
2014-06-22 18:38:40 -0700

    Initial Commit - Empty Activity

---
+++ USGSEarthquakesSonoraFeed/app/src/main/java/com/rcgonzalezf/android/earthquakessonora/SonoraEarthquakesActivity.java
@@ -0,0 +1,19 @@
+package com.rcgonzalezf.android.earthquakessonora;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class SonoraEarthquakesActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sonora_earthquakes);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+}
