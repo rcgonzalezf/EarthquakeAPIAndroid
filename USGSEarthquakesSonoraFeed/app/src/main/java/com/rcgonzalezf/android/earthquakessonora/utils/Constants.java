6823842a5e45dafec72db0b8d130b3107f85c51e
Carlos <iscrobertogonzalez@gmail.com>
2014-08-25 22:31:24 -0700

    Adding a basic Constants class.

---
+++ USGSEarthquakesSonoraFeed/app/src/main/java/com/rcgonzalezf/android/earthquakessonora/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.rcgonzalezf.android.earthquakessonora.utils;
+
+import com.octo.android.robospice.persistence.DurationInMillis;
+
+public interface Constants {
+
+    long APP_CACHE_DURATION = 5 * DurationInMillis.ONE_MINUTE;
+    short DEFAULT_EARTHQUAKE_MAX_ROWS = 20;
+}
