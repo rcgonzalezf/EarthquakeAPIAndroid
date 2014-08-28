5b374ff635495967533737256c3774097bcc912b
Carlos <iscrobertogonzalez@gmail.com>
2014-08-27 23:27:55 -0700

    Setting up Crittercism to check the errors and logs during the fly.

--- USGSEarthquakesSonoraFeed/app/src/main/java/com/rcgonzalezf/android/earthquakessonora/SonoraEarthquakesActivity.java
+++ USGSEarthquakesSonoraFeed/app/src/main/java/com/rcgonzalezf/android/earthquakessonora/SonoraEarthquakesActivity.java
@@ -1,24 +1,10 @@
 package com.rcgonzalezf.android.earthquakessonora;
 
-import android.app.ActionBar;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
 
-import com.octo.android.robospice.persistence.DurationInMillis;
-import com.octo.android.robospice.persistence.exception.SpiceException;
-import com.octo.android.robospice.request.listener.RequestListener;
 import com.rcgonzalezf.android.earthquakessonora.fragments.EarthquakeListFragment;
-import com.rcgonzalezf.android.earthquakessonora.responses.EarthQuakesSonoraMessage;
-import com.rcgonzalezf.android.earthquakessonora.service.EarthQuakesSonoraRequest;
+import com.crittercism.app.Crittercism;
+import com.rcgonzalezf.android.earthquakessonora.utils.Constants;
 
 public class SonoraEarthquakesActivity extends BaseActivity {
 
@@ -27,6 +13,7 @@ public class SonoraEarthquakesActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Crittercism.initialize(getApplicationContext(), Constants.CRITTERCISM_APP_ID);
         setContentView(R.layout.main_layout);
 
         if (getSupportFragmentManager().findFragmentById(R.id.earthquakes_list_placeholder) == null) {
--- USGSEarthquakesSonoraFeed/app/src/main/java/com/rcgonzalezf/android/earthquakessonora/utils/Constants.java
+++ USGSEarthquakesSonoraFeed/app/src/main/java/com/rcgonzalezf/android/earthquakessonora/utils/Constants.java
@@ -6,4 +6,6 @@ public interface Constants {
 
     long APP_CACHE_DURATION = 5 * DurationInMillis.ONE_MINUTE;
     short DEFAULT_EARTHQUAKE_MAX_ROWS = 20;
+
+    String CRITTERCISM_APP_ID = "CRITTERCISM_APP_ID";
 }
